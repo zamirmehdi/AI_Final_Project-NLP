c1212b2704bef5404aef9adee644518fc886585e
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2020-08-09 19:23:57 +0430

    data input from file & parameters creation Added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 file = open('/Users/apple/Desktop/Train_data.txt')
+text = file.readlines()
+number_of_words = 0
 
-i = 0
-for line in file:
-    print(i + 1, ': ', line)
-    i += 1
-    if i > 19:
-        break
+unknown = '<UNK>'
+none = '</s>'
+
+print(len(text))
+print(text[2])
+
+unigram_counter = dict()
+bigram_counter = dict()
+trigram_counter = dict()
+
+unigram_counter[unknown] = 0
+print(unigram_counter)
+
