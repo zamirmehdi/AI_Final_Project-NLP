d95820d06d775bdc874d64508a3351b22cea2b26
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2020-08-10 17:03:33 +0430

    Modularizing counter_calculators and probability_calculators Added

--- main.py
+++ main.py
@@ -13,172 +13,228 @@ unigram_counter = dict()
 bigram_counter = dict()
 trigram_counter = dict()
 
+unigram_probability = dict()
+bigram_probability = dict()
+trigram_probability = dict()
+
 unigram_counter[unknown] = 0
 
-for line in train_text:
 
-    temp_words = line.split(' ')
-    temp_words.remove(temp_words[len(temp_words) - 1])
-    # temp_words[len(temp_words) - 1] = temp_words[len(temp_words) - 1].replace('.\n', '')
-    # print('mmd', temp_words[len(temp_words) - 1], 'mmd')
-    # break
+def unigram_counter_calculator():
+    global unigram_counter
+    global number_of_words
+    for line in train_text:
 
-    for word in temp_words:
+        line.replace('"', '')
+        line.replace(',', '')
+        temp_words = line.split(' ')
+        temp_words.remove(temp_words[len(temp_words) - 1])
+        # temp_words[len(temp_words) - 1] = temp_words[len(temp_words) - 1].replace('.\n', '')
+        # print('mmd', temp_words[len(temp_words) - 1], 'mmd')
+        # break
 
-        number_of_words += 1
+        for word in temp_words:
 
-        if unigram_counter.get(word) is None:
-            unigram_counter[unknown] = unigram_counter.get(unknown) + 1
-            unigram_counter[word] = 1
+            number_of_words += 1
 
-        else:
-            unigram_counter[word] = unigram_counter.get(word) + 1
+            if unigram_counter.get(word) is None:
+                unigram_counter[unknown] = unigram_counter.get(unknown) + 1
+                unigram_counter[word] = 1
 
-print(unigram_counter.get('.\n'))
-print(number_of_words)
-print(sum(unigram_counter.values()))
-print()
+            else:
+                unigram_counter[word] = unigram_counter.get(word) + 1
+
+    print()
+
+    print(unigram_counter.get('.\n'))
+    print(number_of_words)
+    print(len(unigram_counter))
+    print(unigram_counter.get(unknown))
+    print(sum(unigram_counter.values()))
+    print(sum(unigram_counter.values()) - unigram_counter.get(unknown))
+    print()
+
 
 mmd = 0
-for line in train_text:
 
-    temp_words = line.split(' ')
-    temp_words.remove(temp_words[len(temp_words) - 1])
 
-    for i in range(0, len(temp_words) + 1):
+def bigram_counter_calculator():
+    global mmd
+    global bigram_counter
+    for line in train_text:
 
-        last = ''
+        line.replace('"', '')
+        line.replace(',', '')
+        temp_words = line.split(' ')
+        temp_words.remove(temp_words[len(temp_words) - 1])
 
-        if i == 0:
-            current = temp_words[i]
-            last = none
-        elif i == len(temp_words):
-            current = none
-            last = temp_words[len(temp_words) - 1]
-        else:
-            current = temp_words[i]
-            last = temp_words[i - 1]
+        for i in range(0, len(temp_words) + 1):
 
-        if bigram_counter.get((last, current)) is None:
+            last = ''
 
-            if bigram_counter.get((last, unknown)) is None:
-                bigram_counter[last, unknown] = 1
+            if i == 0:
+                current = temp_words[i]
+                last = none
+            elif i == len(temp_words):
+                current = none
+                last = temp_words[len(temp_words) - 1]
             else:
-                bigram_counter[last, unknown] = bigram_counter.get((last, unknown)) + 1
+                current = temp_words[i]
+                last = temp_words[i - 1]
+
+            if bigram_counter.get((last, current)) is None:
+
+                if bigram_counter.get((last, unknown)) is None:
+                    bigram_counter[last, unknown] = 1
+                else:
+                    bigram_counter[last, unknown] = bigram_counter.get((last, unknown)) + 1
+                    mmd += 1
+
+                bigram_counter[last, current] = 1
+
+            else:
+                bigram_counter[last, current] = bigram_counter.get((last, current)) + 1
                 mmd += 1
 
-            bigram_counter[last, current] = 1
+    print(bigram_counter.get(('the', 'U.S.')))
+    print(len(bigram_counter))
+    print(sum(bigram_counter.values()))
+    print(mmd)
+    print()
 
-        else:
-            bigram_counter[last, current] = bigram_counter.get((last, current)) + 1
-            mmd += 1
-
-print(bigram_counter.get(('the', 'U.S.')))
-print(len(bigram_counter))
-print(sum(bigram_counter.values()))
-print(mmd)
-print()
 
 jas = 0
-for line in train_text:
 
-    temp_words = line.split(' ')
-    temp_words.remove(temp_words[len(temp_words) - 1])
 
-    for i in range(0, (len(temp_words) + 1)):
+def trigram_counter_calculator():
+    global trigram_counter
+    global jas
 
-        last = ''
-        last_last = ''
-        if len(temp_words) <= 1:
-            continue
-        if i == 0:
-            current = temp_words[i]
-            last = none
-            last_last = none
-        elif i == 1 and len(temp_words) > 1:
-            current = temp_words[i]
-            last = temp_words[i - 1]
-            last_last = none
-        elif i == len(temp_words):
-            current = none
-            last = temp_words[len(temp_words) - 1]
-            last_last = temp_words[len(temp_words) - 2]
-        else:
-            current = temp_words[i]
-            last = temp_words[i - 1]
-            last_last = temp_words[i - 2]
+    for line in train_text:
 
-        if trigram_counter.get((last_last, last, current)) is None:
-            if trigram_counter.get((last_last, last, unknown)) is None:
-                trigram_counter[last_last, last, unknown] = 1
-                # if temp_words[48] == 'flu' and i == 1:
-                #     print('i:', i)
-                #     print(last_last, last, current)
+        line.replace('"', '')
+        line.replace(', ', '')
+        temp_words = line.split(' ')
+        temp_words.remove(temp_words[len(temp_words) - 1])
+
+        for i in range(0, (len(temp_words) + 1)):
+
+            last = ''
+            last_last = ''
+            if len(temp_words) <= 1:
+                continue
+            if i == 0:
+                current = temp_words[i]
+                last = none
+                last_last = none
+            elif i == 1 and len(temp_words) > 1:
+                current = temp_words[i]
+                last = temp_words[i - 1]
+                last_last = none
+            elif i == len(temp_words):
+                current = none
+                last = temp_words[len(temp_words) - 1]
+                last_last = temp_words[len(temp_words) - 2]
             else:
-                trigram_counter[last_last, last, unknown] = trigram_counter.get((last_last, last, unknown)) + 1
+                current = temp_words[i]
+                last = temp_words[i - 1]
+                last_last = temp_words[i - 2]
+
+            if trigram_counter.get((last_last, last, current)) is None:
+                if trigram_counter.get((last_last, last, unknown)) is None:
+                    trigram_counter[last_last, last, unknown] = 1
+                    # if temp_words[48] == 'flu' and i == 1:
+                    #     print('i:', i)
+                    #     print(last_last, last, current)
+                else:
+                    trigram_counter[last_last, last, unknown] = trigram_counter.get((last_last, last, unknown)) + 1
+                    jas += 1
+
+                trigram_counter[last_last, last, current] = 1
+
+            else:
+                trigram_counter[last_last, last, current] = trigram_counter.get((last_last, last, current)) + 1
                 jas += 1
 
-            trigram_counter[last_last, last, current] = 1
+    print(trigram_counter.get((none, 'The', 'U.S.')))
+    print(len(trigram_counter))
+    print((sum(trigram_counter.values())))
+    print(jas)
+    print()
 
+
+def unigram_probability_calculator():
+    global unigram_probability
+
+    for elem in unigram_counter.keys():
+        unigram_probability[elem] = unigram_counter.get(elem) / number_of_words
+
+
+def bigram_probability_calculator():
+    global bigram_probability
+
+    for elem in bigram_counter.keys():
+        total_amount = 0
+        input_last = elem[0]
+        input_current = elem[1]
+
+        # bigram_probability[elem] = bigram_probability_calculator(elem[0], elem[1])
+        if input_last != none:
+            result = bigram_counter.get((input_last, input_current)) / unigram_counter.get(input_last)
         else:
-            trigram_counter[last_last, last, current] = trigram_counter.get((last_last, last, current)) + 1
-            jas += 1
+            for key in bigram_counter.keys():
+                if key[0] == none:
+                    total_amount += bigram_counter.get(key)
+            result = bigram_counter.get((input_last, input_current)) / total_amount
 
-print(trigram_counter.get((none, 'The', 'U.S.')))
-print(len(trigram_counter))
-print((sum(trigram_counter.values())))
-print(jas)
-print()
+        bigram_probability[elem] = result
 
 
-def unigram_probability_calculator(input_word):
-    return unigram_counter.get(input_word) / number_of_words
+def trigram_probability_calculator():
+    global trigram_probability
 
+    for elem in trigram_counter.keys():
+        total_amount = 0
+        # trigram_probability[elem] = trigram_probability_calculator(elem[0], elem[1], elem[2])
+        input_last_last = elem[0]
+        input_last = elem[1]
+        input_current = elem[2]
 
-def bigram_probability_calculator(input_last, input_current):
-    total_amount = 0
+        if input_last != none:
+            result = (trigram_counter.get((input_last_last, input_last, input_current))) / (bigram_counter.get((
+                (input_last_last, input_last))))
+        else:
+            for key in trigram_counter.keys():
+                if key[1] == none:
+                    total_amount += trigram_counter.get(key)
+            result = trigram_counter.get((input_last_last, input_last, input_current)) / total_amount
 
-    if input_last != none:
-        return bigram_counter.get((input_last, input_current)) / unigram_counter.get(input_last)
-    else:
-        for key in bigram_counter.keys():
-            if key[0] == none:
-                total_amount += bigram_counter.get(key)
-        return bigram_counter.get((input_last, input_current)) / total_amount
-
-
-def trigram_probability_calculator(input_last_last, input_last, input_current):
-    total_amount = 0
-
-    if input_last != none:
-        return (trigram_counter.get((input_last_last, input_last, input_current))) / (bigram_counter.get((
-            (input_last_last, input_last))))
-    else:
-        for key in trigram_counter.keys():
-            if key[1] == none:
-                total_amount += trigram_counter.get(key)
-        return trigram_counter.get((input_last_last, input_last, input_current)) / total_amount
+        trigram_probability[elem] = result
 
 
 # def back_off(l1, l2, l3, ):
 
 
-print(unigram_probability_calculator('Abdullah'))
-print(unigram_counter.get('Abdullah'))
 
-unigram_probability = dict()
-bigram_probability = dict()
-trigram_probability = dict()
+# print(unigram_probability_calculator('Abdullah'))
+# print(unigram_counter.get('Abdullah'))
 
-for elem in unigram_counter.keys():
-    unigram_probability[elem] = unigram_probability_calculator(elem)
 
-print(len(bigram_counter))
-for elem in bigram_counter.keys():
-    bigram_probability[elem] = bigram_probability_calculator(elem[0], elem[1])
+# for elem in unigram_counter.keys():
+#     unigram_probability[elem] = unigram_probability_calculator(elem)
 
-for elem in trigram_counter.keys():
-    trigram_probability[elem] = trigram_probability_calculator(elem[0], elem[1], elem[2])
+# print(len(bigram_counter))
+# for elem in bigram_counter.keys():
+#     bigram_probability[elem] = bigram_probability_calculator(elem[0], elem[1])
+
+# for elem in trigram_counter.keys():
+#     trigram_probability[elem] = trigram_probability_calculator(elem[0], elem[1], elem[2])
+unigram_counter_calculator()
+bigram_counter_calculator()
+trigram_counter_calculator()
+unigram_probability_calculator()
+bigram_probability_calculator()
+trigram_probability_calculator()
 
 print(bigram_probability.get(('King', 'Abdullah')))
 print()
